067a8c97e29785fb4dbaa48fd32e3480e2c46238
Bipro23 <88981554+Bipro23@users.noreply.github.com>
2023-09-25 18:34:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let x=Object.keys(sampleObject);
+	for( let i=0;i<x.length;i++){
+		if(key==x[i]){
+			return true;
+		}
+	}
+	return false;
 }
 
 // Do not change the code below
